24fcdb2120f2b985269d66c94209e07a4668f15d
Nikoo Yekehbash <yeke0002@algonquinlive.com>
2021-12-10 13:09:02 -0500

    The first steps

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    let x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+window.onscroll = function() {mySecondFunction()};
+
+function mySecondFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    document.getElementById("myImg").className = "slideUp";
+  }
+}
+
